d255f179c648c98906829daf7a0979da7c0e335d
yipqp <rdyip892@gmail.com>
2022-12-05 22:29:33 -0800

    Add resizable grid slider

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const gridContainer = document.querySelector(".grid-container");
+const output = document.querySelector(".output");
+const gridSlider = document.querySelector(".grid-slider");
+
+gridSlider.addEventListener("change", updateGrid);
+
+function updateGrid() {
+    let input = gridSlider.value;
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+
+    for (let i = 0; i < input; i++) {
+        let gridRow = makeRow();
+        for (let j = 0; j < input; j++) {
+            const gridSquare = makeSquare();
+            gridRow.appendChild(gridSquare);
+        }  
+        gridContainer.appendChild(gridRow); 
+    }
+}
+
 
 function makeSquare() {
     const gridSquare = document.createElement("div");
     gridSquare.setAttribute("class", "grid-square");
     gridSquare.addEventListener("mouseenter", () => {
-        gridSquare.style.backgroundColor = "purple";
+        gridSquare.style.backgroundColor = "#B9CAC4";
     });
     gridSquare.addEventListener("mouseleave", () => {
         gridSquare.style.backgroundColor = "";
